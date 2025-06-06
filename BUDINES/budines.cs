698707b84274451252dad3af496d3f7ebe028e8f
santiago ansa <santiagoansa4@gmail.com>
2025-06-05 23:12:07 -0300

    entrega de la guia 7-1

---
+++ BUDINES/budines.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUDINES
+{
+    class budines
+    {
+        static double masakg,masagr,restomasa, moldesusados, envoltorios, cajas;
+        static double budinesSinEmpaquetar, paquetesSinCaja;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese la cantidad de masa que producio en kg");
+            masakg = Convert.ToDouble(Console.ReadLine());
+            masagr = masakg * 1000;
+            
+      
+            moldesusados = Math.Truncate(masagr / 55);
+            restomasa = masagr % 55;
+
+            envoltorios =Math.Truncate(moldesusados / 12);
+
+            cajas =Math.Truncate(envoltorios / 20);
+
+             budinesSinEmpaquetar = moldesusados % 12;
+             paquetesSinCaja = envoltorios % 20;
+
+
+
+            Console.WriteLine($"La cantidad de budines producidos: {moldesusados}");
+            Console.WriteLine($"La cantidad de paquetes de budines (envoltorios): {envoltorios}");
+            Console.WriteLine($"La cantidad de cajas llenas listas para distribución: {cajas}");
+            Console.WriteLine($"Masa sobrante después de hacer los budines: {restomasa} gramos");
+            Console.WriteLine("----------------");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Budines sin empaquetar: {budinesSinEmpaquetar}");
+            Console.WriteLine($"Paquetes sin caja: {paquetesSinCaja}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PRUEBA1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PRUEBA1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PRUEBA1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("12b31798-9e8a-4739-888b-59053cc5c8f0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
